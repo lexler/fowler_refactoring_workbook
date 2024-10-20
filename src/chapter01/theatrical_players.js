6bf5ec77f66fe97a85ddd971876f235f45511744
Lada Kesseler <archie.kesseler@gmail.com>
2024-10-19 21:14:34 -0700

    Extract format function and inline format variable

--- src/chapter01/theatrical_players.js
+++ src/chapter01/theatrical_players.js
@@ -2,12 +2,6 @@ function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-        {
-            style: "currency",
-            currency: "USD",
-            minimumFractionDigits: 2
-        }).format;
 
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
@@ -18,7 +12,6 @@ function statement(invoice, plays) {
     }
 
     result += `Amount owed is ${format(totalAmount/100)}\n`;
-
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 
@@ -53,6 +46,15 @@ function statement(invoice, plays) {
         result += Math.max(aPerformance.audience - 30, 0);
         if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
         return result;
+    }
+
+    function format(aNumber) {
+        return new Intl.NumberFormat("en-US",
+            {
+                style: "currency",
+                currency: "USD",
+                minimumFractionDigits: 2
+            }).format(aNumber);
     }
 }
 
