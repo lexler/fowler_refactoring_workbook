f40c97d7d0741a71ce6804f41fb40cddfc75a75d
Lada Kesseler <archie.kesseler@gmail.com>
2024-10-19 22:24:31 -0700

    Implemented renderHtml and added tests for it

--- __tests__/theatrical_players.test.js
+++ __tests__/theatrical_players.test.js
@@ -1,4 +1,4 @@
-const statement = require('../src/chapter01/theatrical_players');
+const { statement, htmlStatement } = require('../src/chapter01/theatrical_players');
 const plays = require('../src/chapter01/plays.json');
 const invoice = require('../src/chapter01/invoices.json');
 
@@ -26,6 +26,36 @@ describe('statement', () => {
 
         expect(() => {
             statement(invoice, invalidPlays);
+        }).toThrow('unknown type: unknownType');
+    });
+});
+
+describe('htmlStatement', () => {
+    it('should return a correct HTML statement for a given invoice and plays', () => {
+        const expectedHtmlStatement =
+            `<h1>Statement for BigCo</h1>\n` +
+            `<table>\n` +
+            `<tr><th>play</th><th>seats</th><th>cost</th></tr>\n` +
+            ` <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n` +
+            ` <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n` +
+            ` <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n` +
+            `</table>\n` +
+            `<p>Amount owed is <em>$1,730.00</em></p>\n` +
+            `<p>You earned <em>47</em> credits</p>\n`;
+
+        const result = htmlStatement(invoice, plays);
+
+        expect(result).toBe(expectedHtmlStatement);
+    });
+
+    it('should throw an error for unknown play type in HTML statement', () => {
+        const invalidPlays = {
+            ...plays,
+            "as-like": { "name": "As You Like It", "type": "unknownType" }
+        };
+
+        expect(() => {
+            htmlStatement(invoice, invalidPlays);
         }).toThrow('unknown type: unknownType');
     });
 });
