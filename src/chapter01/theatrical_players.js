52ecc7ef6db39321727ff4cc25055c806ded77be
Lada Kesseler <archie.kesseler@gmail.com>
2024-10-19 21:26:04 -0700

    Split loop

--- src/chapter01/theatrical_players.js
+++ src/chapter01/theatrical_players.js
@@ -1,11 +1,14 @@
 function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    for (let perf of invoice.performances) {
 
+    let totalAmount = 0;
+    for (let perf of invoice.performances) {
+        totalAmount += amountFor(perf);
+    }
+
+    for (let perf of invoice.performances) {
         // print line for this order
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
-        totalAmount += amountFor(perf);
     }
 
     result += `Amount owed is ${usd(totalAmount/100)}\n`;
