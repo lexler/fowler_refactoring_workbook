6e7996fba4cce075ed69b4f086c30712238f6b38
Lada Kesseler <archie.kesseler@gmail.com>
2024-11-03 10:40:09 -0800

    Got rid of the old amountFor and volumeCredits

--- src/chapter01/createStatementData.js
+++ src/chapter01/createStatementData.js
@@ -71,14 +71,6 @@ function createStatementData(invoice, plays) {
         return plays[aPerformance.playID];
     }
 
-    function amountFor(aPerformance) {
-        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
-    }
-
     function totalAmount(data) {
         return data.performances
             .reduce((total, p) => total + p.amount, 0);
