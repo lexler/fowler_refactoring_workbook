269b80ddbe6e1d24b88d5a61f549253a83b662d1
Lada Kesseler <archie.kesseler@gmail.com>
2024-11-03 09:51:12 -0800

    Turned all volumeCreditsFor into delegating function

--- src/chapter01/createStatementData.js
+++ src/chapter01/createStatementData.js
@@ -29,7 +29,7 @@ class PerformanceCalculator {
     get volumeCredits() {
         let result = 0;
         result += Math.max(this.performance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type) result += Math.floor(this.performance.audience / 5);
+        if ("comedy" === this.performance.play.type) result += Math.floor(this.performance.audience / 5);
         return result;
     }
 }
@@ -61,10 +61,7 @@ function createStatementData(invoice, plays) {
     }
 
     function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-        return result;
+        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
     }
 
     function totalAmount(data) {
