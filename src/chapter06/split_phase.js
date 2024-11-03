6601fe32a35a0f9d5f682e901b8cfc3ba4396dbe
Lada Kesseler <archie.kesseler@gmail.com>
2024-11-03 10:43:22 -0800

    Got rid of redundant comments in chapter6

--- src/chapter06/split_phase.js
+++ src/chapter06/split_phase.js
@@ -1,25 +1,3 @@
-// const orderString = ""
-// const priceList = []
-//
-// const orderData = orderString.split(/\s+/)
-// const productPrice = priceList[orderData[0].split("-")[1]]
-// const orderPrice = parseInt(orderData[1]) * productPrice
-
-
-
-// product = {
-//     "basePrice": 10,
-//     "discountRate": 2,
-//     "discountedFee": 1,
-//     "discountThreshold": 25
-// }
-//
-// shippingMethod = {
-//     "feePerCase": 20,
-//     "discountThreshold": 10,
-//     "discountedFee": 10
-// }
-
 function priceOrder(product, quantity, shippingMethod) {
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0)
@@ -32,6 +10,3 @@ function priceOrder(product, quantity, shippingMethod) {
 }
 
 module.exports = { priceOrder };
-
-// result = priceOrder(product, 100, shippingMethod)
-// console.log(result);
