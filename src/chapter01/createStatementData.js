2d3504c24267371833a7aadba6ed5c2b94d7d761
Lada Kesseler <archie.kesseler@gmail.com>
2024-11-03 09:52:29 -0800

    Fixed error in volumeCredits() and using it in enrichPerformance

--- src/chapter01/createStatementData.js
+++ src/chapter01/createStatementData.js
@@ -29,7 +29,7 @@ class PerformanceCalculator {
     get volumeCredits() {
         let result = 0;
         result += Math.max(this.performance.audience - 30, 0);
-        if ("comedy" === this.performance.play.type) result += Math.floor(this.performance.audience / 5);
+        if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
         return result;
     }
 }
@@ -48,7 +48,7 @@ function createStatementData(invoice, plays) {
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
         result.amount = calculator.amount;
-        result.volumeCredits = volumeCreditsFor(result);
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }
 
