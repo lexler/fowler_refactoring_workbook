0aa5e73bb7cb2647bf7baa81a3e2ff2163192730
Lada Kesseler <archie.kesseler@gmail.com>
2024-11-03 09:01:05 -0800

    moved basePrice to container

--- src/chapter06/split_phase.js
+++ src/chapter06/split_phase.js
@@ -22,16 +22,18 @@ function priceOrder(product, quantity, shippingMethod) {
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0)
         * product.basePrice * product.discountRate;
-    priceData = {}
-    price = applyShipping(priceData, basePrice, shippingMethod, quantity, discount);
+    priceData = {
+        basePrice: basePrice
+    }
+    price = applyShipping(priceData, shippingMethod, quantity, discount);
     return price;
 }
 
-function applyShipping(priceData, basePrice, shippingMethod, quantity, discount) {
-    const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
+function applyShipping(priceData, shippingMethod, quantity, discount) {
+    const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
         ? shippingMethod.discountedFee : shippingMethod.feePerCase;
     const shippingCost = quantity * shippingPerCase;
-    const price = basePrice - discount + shippingCost;
+    const price = priceData.basePrice - discount + shippingCost;
     return price;
 }
 
