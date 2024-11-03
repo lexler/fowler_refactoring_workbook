61c5958e9a15c441aeeb22543830f51bfbf3c2ee
Lada Kesseler <archie.kesseler@gmail.com>
2024-11-03 09:06:06 -0800

    Extract function for the first phase returning the data object introduced

--- src/chapter06/split_phase.js
+++ src/chapter06/split_phase.js
@@ -19,16 +19,20 @@
 // }
 
 function priceOrder(product, quantity, shippingMethod) {
+    const priceData = calculatePricingData(product, quantity);
+    price = applyShipping(priceData, shippingMethod);
+    return price;
+}
+
+function calculatePricingData(product, quantity) {
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0)
         * product.basePrice * product.discountRate;
-    priceData = {
+    return {
         basePrice: basePrice,
         quantity: quantity,
         discount: discount
     }
-    price = applyShipping(priceData, shippingMethod);
-    return price;
 }
 
 function applyShipping(priceData, shippingMethod) {
